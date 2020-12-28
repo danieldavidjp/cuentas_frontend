16ec449b49816bb73de5d055583be13c22873820
jesus garcia <jegarba1996@gmail.com>
2020-12-28 18:20:57 -0500

    quitado el form de transacciones agregada la vista de transferir Dinero

---
+++ BANCO/CNTAS/ADMIN/forms/T_CNTASIQOTREKI_837/events/executeQuery/Q_CUENETNT_QR74.js
@@ -0,0 +1,6 @@
+
+//CuentaDestinoQuery Entity: CuentaDestino
+task.executeQuery.Q_CUENETNT_QR74 = function(executeQueryEventArgs){
+    executeQueryEventArgs.commons.execServer = true;
+    //executeQueryEventArgs.commons.serverParameters.CuentaDestino = true;
+};
---
+++ BANCO/CNTAS/ADMIN/forms/T_CNTASIQOTREKI_837/events/executeQuery/Q_CUENTAAT_QZ46.js
@@ -0,0 +1,6 @@
+
+//CuentaQuery Entity: Cuenta
+task.executeQuery.Q_CUENTAAT_QZ46 = function(executeQueryEventArgs){
+    executeQueryEventArgs.commons.execServer = true;
+    //executeQueryEventArgs.commons.serverParameters.Cuenta = true;
+};
