cf5530fa12fb89799713e60ceb752f266d376813
Daniel Montes <daniel.montes2682@gmail.com>
2020-12-29 16:45:20 -0500

    Modificaciones en el cliente para mostrar y ocultar groups segun disponibilidad de Cliente en formNuevaCuenta

--- BANCO/CNTAS/ADMIN/forms/T_CNTASADQLTLAP_450/events/executeCommand/VA_VAIMAGEBUTTONNN_740241_Cbk.js
+++
@@ -1,4 +0,0 @@
-
-//Start signature to Callback event to VA_VAIMAGEBUTTONNN_740241
-task.executeCommandCallback.VA_VAIMAGEBUTTONNN_740241 = function(entities, executeCommandCallbackEventArgs) {
-};
--- BANCO/CNTAS/ADMIN/forms/T_CNTASADQLTLAP_450/events/executeQuery/Q_CLIETNNT_ZN50_Cbk.js
+++ BANCO/CNTAS/ADMIN/forms/T_CNTASADQLTLAP_450/events/executeQuery/Q_CLIETNNT_ZN50_Cbk.js
@@ -2,4 +2,10 @@
 //Start signature to Callback event to Q_CLIETNNT_ZN50
 task.executeQueryCallback.Q_CLIETNNT_ZN50 = function(entities, executeQueryCallbackEventArgs) {
      //here your code
+    if(entities.Cliente._data.length > 0) {
+        executeQueryCallbackEventArgs.commons.api.viewState.show('G_NUEVACUAAT_999241')
+    } else {
+        executeQueryCallbackEventArgs.commons.api.viewState.hide('G_NUEVACUAAT_999241')
+ 
+    }
 };
