c327cd3c681e45ff15e27345bf4084bfde1b3d9d
Daniel Jimenez <danieldavidjp@gmail.com>
2020-12-29 15:39:28 -0500

    Agregado queries y blis + query.impl

---
+++ BANCO/CNTAS/ADMIN/forms/T_CNTASPIVDVWDH_706/events/executeCommand/VA_VAIMAGEBUTTONNN_548102_Cbk.js
@@ -0,0 +1,5 @@
+
+//Start signature to Callback event to VA_VAIMAGEBUTTONNN_548102
+task.executeCommandCallback.VA_VAIMAGEBUTTONNN_548102 = function(entities, executeCommandCallbackEventArgs) {
+     //here your code
+};
