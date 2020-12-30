090565e0091ac85873f6b1bfd41e9f989074fcad
jesus garcia <jegarba1996@gmail.com>
2020-12-29 20:09:10 -0500

    commit de seguridad error de parseo por double

--- BANCO/CNTAS/ADMIN/forms/T_CNTASIQOTREKI_837/events/executeQuery/Q_CUENTAAT_QZ46.js
+++ BANCO/CNTAS/ADMIN/forms/T_CNTASIQOTREKI_837/events/executeQuery/Q_CUENTAAT_QZ46.js
@@ -1,6 +1,7 @@
 
 //CuentaQuery Entity: Cuenta
 task.executeQuery.Q_CUENTAAT_QZ46 = function(executeQueryEventArgs){
+    executeQueryEventArgs.parameters.cuenta=executeQueryEventArgs.commons.api.vc.model.FiltroCuenta.numeroCuenta;
     executeQueryEventArgs.commons.execServer = true;
     //executeQueryEventArgs.commons.serverParameters.Cuenta = true;
 };
