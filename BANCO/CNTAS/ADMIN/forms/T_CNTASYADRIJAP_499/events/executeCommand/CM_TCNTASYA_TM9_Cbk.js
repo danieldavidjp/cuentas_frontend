0cea69546db2118e9239e41624c057c16c1eda89
Daniel Jimenez <danieldavidjp@gmail.com>
2020-12-29 20:15:08 -0500

    Agregado CallBack boton retiro

---
+++ BANCO/CNTAS/ADMIN/forms/T_CNTASYADRIJAP_499/events/executeCommand/CM_TCNTASYA_TM9_Cbk.js
@@ -0,0 +1,5 @@
+
+//Start signature to Callback event to CM_TCNTASYA_TM9
+task.executeCommandCallback.CM_TCNTASYA_TM9 = function(entities, executeCommandCallbackEventArgs) {
+     //here your code
+};
