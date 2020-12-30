ff20b1a58470890e199c41dc05621d3681ac255a
Daniel Jimenez <danieldavidjp@gmail.com>
2020-12-30 09:34:25 -0500

    Correccion de errores sobre pantalla de retiro, agregado al callback del boton y del query

--- BANCO/CNTAS/ADMIN/forms/T_CNTASYADRIJAP_499/events/executeCommand/CM_TCNTASYA_TM9_Cbk.js
+++ BANCO/CNTAS/ADMIN/forms/T_CNTASYADRIJAP_499/events/executeCommand/CM_TCNTASYA_TM9_Cbk.js
@@ -2,4 +2,5 @@
 //Start signature to Callback event to CM_TCNTASYA_TM9
 task.executeCommandCallback.CM_TCNTASYA_TM9 = function(entities, executeCommandCallbackEventArgs) {
      //here your code
+    executeCommandCallbackEventArgs.commons.api.grid.refresh('QV_IM74_SDK14');
 };
--- BANCO/CNTAS/ADMIN/forms/T_CNTASYADRIJAP_499/events/render/VC_RETIROYQZC_916499.js
+++ BANCO/CNTAS/ADMIN/forms/T_CNTASYADRIJAP_499/events/render/VC_RETIROYQZC_916499.js
@@ -3,7 +3,7 @@
 //Evento render : Se ejecuta antes de renderizar un control, permite realizar personalizaciones visuales
 //ViewContainer: FormRetiro
 task.render = function (entities, renderEventArgs){
-
+    
     renderEventArgs.commons.api.viewState.hide('VA_SALDOSTJCRNVDVD_624995');
     renderEventArgs.commons.api.viewState.hide('CM_TCNTASYA_TM9');
     renderEventArgs.commons.execServer = false;
